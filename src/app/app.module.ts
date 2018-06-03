3e1ce11e21f9e14d1f419081e43d2207f58b4466
Alexandr Plokhih <s.plohoj@gmail.com>
2018-06-03 16:47:13 +0300

    converting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ConverterComponent } from './components/converter/converter.component';
@@ -7,16 +8,17 @@ import { AddButtonComponent } from './shared/add-button/add-button.component';
 import { GroupComponent } from './shared/group/group.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddButtonComponent,
-    ConverterComponent,
-    GroupComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AddButtonComponent,
+		ConverterComponent,
+		GroupComponent,
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -7,10 +7,62 @@ import { IConverterSize } from '../../interfaces/iconverter-size';
 	styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent implements OnInit {
-	@Input() public coverter: IConverterSize = {width: 1920, height: 320};
+	@Input() public set converter(size: IConverterSize) {
+		this.values.width = size.width;
+		this.height = size.height;
+	}
+	public values = {
+		width: 1920,
+		height: 1080,
+		px: null as number,
+		vw: null as number,
+		vh: null as number,
+	};
 	constructor() { }
 
-	ngOnInit() {
+	public set width(value: number) {
+		this.values.width = value;
+		this.updatePX();
 	}
-
+	public get width() {
+		return this.values.width;
+	}
+	public set height(value: number) {
+		this.values.height = value;
+		this.updatePX();
+	}
+	public get height() {
+		return this.round(this.values.height);
+	}
+	public set px(value: number) {
+		this.values.px = value;
+		this.updatePX();
+	}
+	public get px() {
+		return this.round(this.values.px);
+	}
+	public set vw(value: number) {
+		this.values.vw = value;
+		this.values.px = this.values.width * 0.01 * value;
+		this.values.vh = this.values.px / this.values.height * 100;
+	}
+	public get vw() {
+		return this.round(this.values.vw);
+	}
+	public set vh(value: number) {
+		this.values.vh = value;
+		this.values.px = this.values.height * 0.01 * value;
+		this.values.vw = this.values.px / this.values.width * 100;
+	}
+	public get vh() {
+		return this.round(this.values.vh);
+	}
+	private updatePX() {
+		this.values.vw = this.values.px / this.values.width * 100;
+		this.values.vh = this.values.px / this.values.height * 100;
+	}
+	private round(value: number) {
+		return (Math.round(value * 100) * 0.01);
+	}
+	ngOnInit() {}
 }
