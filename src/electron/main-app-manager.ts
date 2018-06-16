08e15bbede3b5a5fdc77db7dd8a22d26f3ec9799
Alexandr Plokhih <s.plohoj@gmail.com>
2018-06-16 16:24:11 +0300

    Add menu options

--- src/electron/main-app-manager.ts
+++ src/electron/main-app-manager.ts
@@ -1,5 +1,6 @@
-import { BrowserWindow, app } from 'electron';
+
 import { ApplicationMode } from './interfaces/application-mode';
+import { Menu, BrowserWindow, MenuItem, MenuItemConstructorOptions } from 'electron';
 
 const PATH = __dirname;
 
@@ -8,11 +9,34 @@ export class MainAppManager {
 		width: 500,
 		height: 500,
 	});
+	private menu = new Menu();
+	private options = new MenuItem({
+		label: 'Опции',
+		submenu: [
+			{
+				type: 'checkbox',
+				label: 'Поверх окон',
+				click: (option) => this.toggleAlwaysOnTop(option.checked),
+			}, {
+				id: 'devTools',
+				type: 'checkbox',
+				label: 'Панель разработчика',
+				accelerator: 'F12',
+				click: this.updateShowDevTools.bind(this),
+			}, {
+				label: 'Выход',
+				click: () => this.mainWindow.close(),
+			}
+		],
+	});
+
 	constructor(mode: ApplicationMode) {
+		this.menu.insert(0, this.options);
+		this.mainWindow.setMenu(this.menu);
 		switch (mode) {
 			case 'NG_SERVE':
-				this.mainWindow.loadURL(`http://localhost:4200`);
-				this.mainWindow.webContents.openDevTools();
+				this.mainWindow.loadURL('http://localhost:4200');
+				this.isShowDevTools = true;
 				break;
 			case 'FiLE_SYSTEM':
 			default:
@@ -20,5 +44,29 @@ export class MainAppManager {
 				break;
 		}
 		console.log(`Open main window, mode: ${mode}`);
+	}
+	private set isShowDevTools(show: boolean) {
+		const devTools = this.menu.getMenuItemById('devTools')
+		if (devTools) {
+			devTools.checked = show;
+		}
+		this.updateShowDevTools();
+	}
+	private get isShowDevTools() {
+		const devTools = this.menu.getMenuItemById('devTools')
+		if (devTools) {
+			return devTools.checked;
+		}
+		return false;
+	}
+	private updateShowDevTools() {
+		if (this.isShowDevTools) {
+			this.mainWindow.webContents.openDevTools();
+		} else {
+			this.mainWindow.webContents.closeDevTools();
+		}
+	}
+	private toggleAlwaysOnTop(showOnTop: boolean) {
+		this.mainWindow.setAlwaysOnTop(showOnTop);
 	}
 }
