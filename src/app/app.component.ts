09abedc580de2041f89ef79b77ed44e5b4ffcf07
Alexandr Plokhih <s.plohoj@gmail.com>
2018-06-17 22:01:07 +0300

    Automatic save converter sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,30 @@ export class AppComponent implements OnInit {
 	public converters: IConverterSize[] = [];
 
 	ngOnInit() {
-
+		const convertersString = localStorage.getItem('converters');
+		if (convertersString) {
+			const converters = JSON.parse(convertersString);
+			if (converters instanceof Array) {
+				this.converters = converters;
+			}
+		}
+		window.addEventListener('unload', this.saveConverters.bind(this));
 	}
 	public add() {
 		this.converters.push({width: null, height: null});
+	}
+	public delete(item: IConverterSize) {
+		const index = this.converters.findIndex(converter => converter === item);
+		if (index >= 0) {
+			this.converters.splice(index, 1);
+		}
+	}
+	public modification(item: IConverterSize, size: IConverterSize) {
+		item.width = size.width;
+		item.height = size.height;
+		this.saveConverters();
+	}
+	public saveConverters() {
+		localStorage.setItem('converters', JSON.stringify(this.converters));
 	}
 }
--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { IConverterSize } from '../../interfaces/iconverter-size';
 import { BufferUnitService } from '../../shared/services/buffer-unit.service';
 
@@ -8,32 +8,41 @@ import { BufferUnitService } from '../../shared/services/buffer-unit.service';
 	styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent implements OnInit {
-	@Input() public set converter(size: IConverterSize) {
-		this.values.width = size.width;
-		this.height = size.height;
-	}
-	public values = {
+
+	public _size: IConverterSize = {
 		width: 1920,
 		height: 1080,
+	};
+	@Input() public set size(value: IConverterSize) {
+		this._size.width = value.width;
+		this.height = value.height;
+	}
+	@Output() public modification = new EventEmitter<IConverterSize>();
+	@Output() public delete = new EventEmitter();
+	public values = {
 		px: null as number,
 		vw: null as number,
 		vh: null as number,
 	};
 	constructor(private bufferUnitService: BufferUnitService) {}
-
+	public get size() {
+		return this._size;
+	}
 	public set width(value: number | string) {
-		this.values.width = typeof value === 'string' ? parseFloat(value) : value;
+		this.size.width = typeof value === 'string' ? parseFloat(value) : value;
+		this.modification.next(this.size);
 		this.updatePX();
 	}
 	public get width() {
-		return this.round(this.values.width);
+		return this.round(this.size.width);
 	}
 	public set height(value: number | string) {
-		this.values.height = typeof value === 'string' ? parseFloat(value) : value;
+		this.size.height = typeof value === 'string' ? parseFloat(value) : value;
+		this.modification.next(this.size);
 		this.updatePX();
 	}
 	public get height() {
-		return this.round(this.values.height);
+		return this.round(this.size.height);
 	}
 	public set px(value: number | string) {
 		this.values.px = typeof value === 'string' ? parseFloat(value) : value;
@@ -45,8 +54,8 @@ export class ConverterComponent implements OnInit {
 	public set vw(value: number | string) {
 		const valueNumber = typeof value === 'string' ? parseFloat(value) : value;
 		this.values.vw = valueNumber;
-		this.values.px = this.values.width * 0.01 * valueNumber;
-		this.values.vh = this.values.px / this.values.height * 100;
+		this.values.px = this.size.width * 0.01 * valueNumber;
+		this.values.vh = this.values.px / this.size.height * 100;
 	}
 	public get vw() {
 		return this.round(this.values.vw);
@@ -54,17 +63,20 @@ export class ConverterComponent implements OnInit {
 	public set vh(value: number | string) {
 		const valueNumber = typeof value === 'string' ? parseFloat(value) : value;
 		this.values.vh = valueNumber;
-		this.values.px = this.values.height * 0.01 * valueNumber;
-		this.values.vw = this.values.px / this.values.width * 100;
+		this.values.px = this.size.height * 0.01 * valueNumber;
+		this.values.vw = this.values.px / this.size.width * 100;
 	}
 	public get vh() {
 		return this.round(this.values.vh);
 	}
 
-	ngOnInit() {}
+	public ngOnInit() {
+		console.log('init');
+	}
+
 	private updatePX() {
-		this.values.vw = this.values.px / this.values.width * 100;
-		this.values.vh = this.values.px / this.values.height * 100;
+		this.values.vw = this.values.px / this.size.width * 100;
+		this.values.vh = this.values.px / this.size.height * 100;
 	}
 	private round(value: number) {
 		if (value && typeof value === 'number' && value !== Infinity) {
@@ -91,5 +103,8 @@ export class ConverterComponent implements OnInit {
 		}
 		event.clipboardData.setData('text/plain', value + units);
 		event.preventDefault();
+	}
+	onDelete() {
+		this.delete.next();
 	}
 }
